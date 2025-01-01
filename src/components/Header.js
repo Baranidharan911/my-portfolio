fd8a3ad527bed2f764934866716cfbad3f064462
Baranidharan911 <bbharanidharan43@gmail.com>
2025-01-01 22:41:15 +0530

    final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,36 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <header className="header">
       <div className="container">
         <Link to="/" className="logo">Barani</Link>
-        <nav className="nav">
+        <button className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </button>
+        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
           <ul className="nav-list">
             <li><Link to="/">HOME</Link></li>
             <li><Link to="/about">ABOUT</Link></li>
             <li><Link to="/services">SERVICES</Link></li>
             <li className="dropdown" 
-                onMouseEnter={() => setIsOpen(true)}
-                onMouseLeave={() => setIsOpen(false)}
+                onMouseEnter={() => setIsDropdownOpen(true)}
+                onMouseLeave={() => setIsDropdownOpen(false)}
             >
-                <span className="dropdown-trigger">
-                    PROJECTS <span className="arrow">▼</span>
-                </span>
-                {isOpen && (
-                    <ul className="dropdown-menu">
-                        <li><Link to="/works">ALL PROJECTS</Link></li>
-                        <li><Link to="/projects">PROJECT CATEGORIES</Link></li>
-                    </ul>
-                )}
+              <span className="dropdown-trigger">
+                PROJECTS <span className="arrow">▼</span>
+              </span>
+              {isDropdownOpen && (
+                <ul className="dropdown-menu">
+                  <li><Link to="/works">ALL PROJECTS</Link></li>
+                  <li><Link to="/projects">PROJECT CATEGORIES</Link></li>
+                </ul>
+              )}
             </li>
             <li><Link to="/contact">CONTACT</Link></li>
           </ul>
